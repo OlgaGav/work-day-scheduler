aeb755464539d428db6376a006b54b88d8fdb6d9
Olga Gavrushenko <ogavby@gmail.com>
2022-10-14 11:18:49 -0700

    added notification after save

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,30 @@
+const workingHours = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
 var now = dayjs();
 var currentDate = now.format('dddd, MMMM Do');
-var dateStamp = now.format('MM-DD-YY');
 var currentTime = now.format('hA');
 var idCurrentTime;
-var storedSchedule;
+var container = $('.container');
+
+// render current date in header
 $('#currentDay').text(currentDate);
 
-const workingHours = ['9AM','10AM','11AM','12PM','1PM','2PM','3PM','4PM','5PM'];
-
+// idCurrentTime variable is used to add color-coding in textarea
 $.each(workingHours, function(i, hours) {
     if (String(hours) === String(currentTime)){
         idCurrentTime = i;
     }
 }) 
 
-var container = $('.container');
-
+// render table with working hours. Each element has unique id to simplify access of the elements and content
 $.each(workingHours, function (i, hours) {
     container.append('<div class="row time-block" id='+hours+'></div>');
     let row = $('div#'+hours);
-    row.append('<div class="hour text-center col-1" id=t'+i+'></div>');
+    row.append('<div class="hour text-center col-1 col-sm-1" id=t'+i+'></div>');
     $('div#t'+i).text(hours);
-    row.append('<textarea class="description col-10" id=d'+i+'></textarea>');
-    row.append('<button class="saveBtn col-1" id='+i+'></button');
+    row.append('<textarea class="description col-10 col-sm-10" id=d'+i+'></textarea>');
+    row.append('<button class="saveBtn col-1 col-sm-1" id='+i+'></button');
     $('button.saveBtn#'+i).text('🖪');
+    // event listener for save button, and save to localStorage on click
     $('button.saveBtn#'+i).on('click', function(e){
       let index = e.currentTarget.id;
       let eventText = $('textarea#d'+index).val();
@@ -33,8 +34,9 @@ $.each(workingHours, function (i, hours) {
         }
         temp[i] = eventText;
         localStorage.setItem('schedule',JSON.stringify(temp));
+        $("div#notification-area").slideDown().delay(2000).slideUp();
     })
-
+    // add classes for color-coding to indicate whether it is in the past, present, or future
     if (i < idCurrentTime) {
         $('textarea#d'+i).addClass('past');
     } else if (i === idCurrentTime) {
@@ -44,16 +46,13 @@ $.each(workingHours, function (i, hours) {
     }
 }) 
 
-// ToDo: render schedule from local storage
+// Render schedule records from local storage if exist
 $(document).ready(function() {
-    storedSchedule = JSON.parse(localStorage.getItem('schedule'))
-    console.log("storedSchedule", storedSchedule);
+    let storedSchedule = JSON.parse(localStorage.getItem('schedule'));
     if (storedSchedule === null) {
         return;
     }
     $.each(storedSchedule, function(i, event) {
-        console.log("i:", i);
-        console.log("event", event);
         $('textarea#d'+i).val(event);
     })
 })
